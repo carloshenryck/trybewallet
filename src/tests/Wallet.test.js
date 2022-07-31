76751cc70ea62335d65f80f1ea6553c08951f2e9
Henryck <ryck302@gmail.com>
2022-07-31 16:30:41 -0300

    requisito 10

--- src/tests/Wallet.test.js
+++ src/tests/Wallet.test.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { screen } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
 
 import Wallet from '../pages/Wallet';
+
+import mockData from './helpers/mockData';
 
 import { renderWithRouterAndRedux } from './helpers/renderWith';
 
 describe('Página carteira', () => {
+  afterEach(() => jest.clearAllMocks());
+
   it('verifica se o email, valor e moeda está na tela', () => {
     const initialStateMock = {
       user: {
@@ -29,11 +34,94 @@ describe('Página carteira', () => {
 
   it('verifica se os inputs para adicionar uma despesa está na tela ', () => {
     renderWithRouterAndRedux(<Wallet />);
+    const value = screen.getByTestId('value-input');
+    const description = screen.getByTestId('description-input');
+    const currency = screen.getByTestId('currency-input');
+    const method = screen.getByTestId('method-input');
+    const tag = screen.getByTestId('tag-input');
 
-    expect(screen.getByTestId('value-input')).toBeInTheDocument();
-    expect(screen.getByTestId('description-input')).toBeInTheDocument();
-    expect(screen.getByTestId('currency-input')).toBeInTheDocument();
-    expect(screen.getByTestId('method-input')).toBeInTheDocument();
-    expect(screen.getByTestId('tag-input')).toBeInTheDocument();
+    expect(value).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+    expect(currency).toBeInTheDocument();
+    expect(method).toBeInTheDocument();
+    expect(tag).toBeInTheDocument();
   });
+
+  it('verifica se ao adicionar uma despesa, o header é atualizado e adicionado a tabela', async () => {
+    const data = mockData;
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(data),
+    })
+
+    renderWithRouterAndRedux(<Wallet />);
+
+    const value = screen.getByTestId('value-input');
+    const description = screen.getByTestId('description-input');
+    const addButton = screen.getByRole('button', { name: 'Adicionar despesa' });
+
+    userEvent.type(value, '2');
+    userEvent.type(description, 'teste');
+    userEvent.click(addButton);
+    
+    await screen.findAllByText('9.51');
+    expect(screen.getByTestId('total-field').innerHTML).toBe('9.51');
+
+    expect(screen.getByRole('cell', { name: 'teste' }));
+    expect(screen.getByRole('cell', { name: '9.51' }));
+  });
+
+  it('verifca se ao remover a despesa o header e a tabela é atualizado', async () => {
+    const data = mockData;
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(data),
+    })
+
+    renderWithRouterAndRedux(<Wallet />);
+
+    const value = screen.getByTestId('value-input');
+    const description = screen.getByTestId('description-input');
+    const addButton = screen.getByRole('button', { name: 'Adicionar despesa' });
+    
+    userEvent.type(value, '2');
+    userEvent.type(description, 'teste');
+    userEvent.click(addButton);
+    
+    await screen.findAllByText('9.51');
+
+    const deleteButton = screen.getByTestId('delete-btn');
+    userEvent.click(deleteButton);
+
+   const text = await screen.queryByText('9.51');
+   expect(text).toBeNull();
+  })
+
+  it('verifica se a edição atualiza a tabela  e o header', async () => {
+    const data = mockData;
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(data),
+    })
+
+    renderWithRouterAndRedux(<Wallet />);
+
+    const value = screen.getByTestId('value-input');
+    const description = screen.getByTestId('description-input');
+    const addButton = screen.getByRole('button', { name: 'Adicionar despesa' });
+    
+    userEvent.type(value, '2');
+    userEvent.type(description, 'teste');
+    userEvent.click(addButton);
+    
+    await screen.findAllByText('9.51');
+
+    const editButton = screen.getByTestId('edit-btn');
+    userEvent.click(editButton);
+    const saveEditButton = screen.getByRole('button', { name: 'Editar despesa' });
+    userEvent.click(saveEditButton);
+
+   const text = await screen.queryByText('9.51');
+   expect(text).toBeNull();
+  })
 });
